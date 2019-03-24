abc1cb16b13f03f3c47f3d7bca1a349ae8e89154
Noah <noaholatoye@gmail.com>
2019-03-24 20:04:15 +0100

    Complete Project

---
+++ Currency_Converter/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.ComboBoxUsd = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBoxGdp = New System.Windows.Forms.ComboBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(225, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(302, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Currency Coverter (USD - GDP)"
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(297, 97)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(134, 47)
+        Me.txtInput.TabIndex = 1
+        '
+        'ComboBoxUsd
+        '
+        Me.ComboBoxUsd.FormattingEnabled = True
+        Me.ComboBoxUsd.Location = New System.Drawing.Point(297, 169)
+        Me.ComboBoxUsd.Name = "ComboBoxUsd"
+        Me.ComboBoxUsd.Size = New System.Drawing.Size(134, 21)
+        Me.ComboBoxUsd.TabIndex = 3
+        Me.ComboBoxUsd.Text = "USD"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(203, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Enter Amount"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(203, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "From"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(203, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "To"
+        '
+        'ComboBoxGdp
+        '
+        Me.ComboBoxGdp.FormattingEnabled = True
+        Me.ComboBoxGdp.Location = New System.Drawing.Point(297, 214)
+        Me.ComboBoxGdp.Name = "ComboBoxGdp"
+        Me.ComboBoxGdp.Size = New System.Drawing.Size(134, 21)
+        Me.ComboBoxGdp.TabIndex = 6
+        Me.ComboBoxGdp.Tag = ""
+        Me.ComboBoxGdp.Text = "GDP"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(297, 251)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 8
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(764, 450)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBoxGdp)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBoxUsd)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents ComboBoxUsd As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBoxGdp As ComboBox
+    Friend WithEvents btnConvert As Button
+End Class
---
+++ Currency_Converter/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+
+    End Sub
+
+    Private Sub ComboBoxUsd_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxUsd.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ComboBoxGdp_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxGdp.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        'Fixed exchange rate
+        Const gbpRate = 0.76
+
+        'Multiply user input with the current GBP rate
+        MessageBox.Show("$" & txtInput.Text & " to British Pound (at the rate of $0.76) = £" & txtInput.Text * gbpRate)
+        'The txtInput is the converted user input
+
+
+    End Sub
+End Class
